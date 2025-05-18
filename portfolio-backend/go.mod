ad7ee0478c2914444205bc67370d05736aad8303
seccret404 <edwardtua25@gmail.com>
2025-05-18 16:05:40 +0700

    Upload struktur BE

--- portfolio-backend/go.mod
+++ portfolio-backend/go.mod
@@ -1,15 +1,16 @@
-module github.com/seccret404/portfolio-backend
+module github.com/seccret404/portofolio-backend
 
 go 1.23.1
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/gofiber/fiber/v2 v2.52.6 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/lib/pq v1.10.9
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
@@ -18,4 +19,7 @@ require (
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
+	gorm.io/gorm v1.26.1 // indirect
 )
--- portfolio-backend/main.go
+++ portfolio-backend/main.go
@@ -1,54 +1,26 @@
 package main
 
 import (
-	"database/sql"
 	"log"
 	"os"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	"github.com/seccret404/portofolio-backend/config"
 )
 
 func main() {
-	// Load variabel dari .env
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file:", err)
+	if err := godotenv.Load(); err != nil{
+		log.Fatal("Error load env")
 	}
+	 config.ConnectDB()
 
-	// Ambil koneksi string dari env
-	dbSource := os.Getenv("DB_SOURCE")
-	if dbSource == "" {
-		log.Fatal("DB_SOURCE tidak ditemukan di .env")
-	}
-
-	// Koneksi ke PostgreSQL
-	db, err := sql.Open("postgres", dbSource)
-	if err != nil {
-		log.Fatal("Gagal konek ke database:", err)
-	}
-	defer db.Close()
-
-	// Test koneksi
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Database tidak bisa di-ping:", err)
-	}
-	log.Println("✅ Connected to PostgreSQL")
-
-	// Setup Fiber
-	app := fiber.New()
-
-	// Endpoint tes aja dulu
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Server jalan 🚀")
-	})
-
-	// Jalankan server
 	port := os.Getenv("PORT")
-	if port == "" {
+	if port == ""{
 		port = "3000"
 	}
-	log.Println("🚀 Server running at http://localhost:" + port)
+
+	app := fiber.New()
+
+	log.Printf("Server running on port %s", port)
 	log.Fatal(app.Listen(":" + port))
 }
